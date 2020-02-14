a94b7f0c0ea0d258f318f214787b85dd6e3ccde5
ThaiEasyElec <support@thaieasyelec.com>
2020-02-14 16:08:22 +0700

    Add files via upload

---
+++ ESPino_With_PMSX003/ESPino_With_PMS_Basic.ino
@@ -0,0 +1,33 @@
+#include "PMS.h"
+#include <SoftwareSerial.h>
+
+static const int RXPin = 0, TXPin = 2;
+SoftwareSerial PMS_Serial(RXPin, TXPin);
+
+PMS pms(PMS_Serial);
+PMS::DATA data;
+
+void setup()
+{
+  Serial.begin(9600);
+  PMS_Serial.begin(9600);
+}
+
+void loop()
+{
+  if (pms.read(data))
+  {
+    Serial.print("PM 1.0 (ug/m3): ");
+    Serial.println(data.PM_AE_UG_1_0);
+
+    Serial.print("PM 2.5 (ug/m3): ");
+    Serial.println(data.PM_AE_UG_2_5);
+
+    Serial.print("PM 10.0 (ug/m3): ");
+    Serial.println(data.PM_AE_UG_10_0);
+
+    Serial.println();
+  }
+
+  // Do other stuff...
+}
